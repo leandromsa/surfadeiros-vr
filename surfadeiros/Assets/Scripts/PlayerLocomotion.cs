b1448ed7310e235b4eafa23d90e2171b2f239c3c
Leandro Sá <leandrosa.esaf@gmail.com>
2021-07-08 18:01:31 +0100

    Testing water shader

--- surfadeiros/Assets/Scripts/PlayerLocomotion.cs
+++ surfadeiros/Assets/Scripts/PlayerLocomotion.cs
@@ -18,7 +18,10 @@ public class PlayerLocomotion : MonoBehaviour
         //transform.Rotate(0f, Input.acceleration.x * acceleratorSensitivity, 0f);
         //Debug.Log(Input.acceleration.x);
         //Debug.Log( Mathf.Clamp(Camera.main.transform.rotation.z, -10 ,10) * 2f);
-        _rb.velocity = new Vector3(-Mathf.Clamp(Camera.main.transform.rotation.z, -30 ,30) * 4f, 0, 1f);
+        //transform.Rotate(new Vector3(0, Camera.main.transform.rotation.y,0), Space.World);
+        var camRight = new Vector3(Camera.main.transform.right.x,0f, Camera.main.transform.right.z);
+        var camForward = new Vector3(Camera.main.transform.forward.x,0f, Camera.main.transform.forward.z);
+        _rb.velocity = camForward * 3f  + camRight * -Mathf.Clamp(Camera.main.transform.rotation.z, -30 ,30) * 12f;
         //_rb.velocity = new Vector3(0, 0, 2f);
         //transform.position = transform.position + Camera.main.transform.forward * 0.02f;
         //transform.position = transform.position + Camera.main.transform.right * Input.acceleration.x * 0.2f;
