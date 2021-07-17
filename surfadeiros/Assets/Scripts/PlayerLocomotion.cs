9ac25643b555bdb684d8d8f54b8f6ead6add3c85
Leandro Sá <leandrosa.esaf@gmail.com>
2021-07-17 16:18:31 +0100

    Add new locomation tilt system

--- surfadeiros/Assets/Scripts/PlayerLocomotion.cs
+++ surfadeiros/Assets/Scripts/PlayerLocomotion.cs
@@ -7,6 +7,8 @@ public class PlayerLocomotion : MonoBehaviour
 
     public float boardSpeed = 3f;
     public float boardHandle = 12f;
+    [SerializeField]
+    GameObject boardObject;
 
     Rigidbody _rb;
     // Start is called before the first frame update
@@ -18,8 +20,40 @@ public class PlayerLocomotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var camRight = new Vector3(Camera.main.transform.right.x,0f, Camera.main.transform.right.z);
-        var camForward = new Vector3(Camera.main.transform.forward.x,0f, Camera.main.transform.forward.z);
-        _rb.velocity = camForward * boardSpeed  + camRight * -Mathf.Clamp(Camera.main.transform.rotation.z, -30 ,30) * boardHandle;
+        // LOCOMOTION
+        var camRight = new Vector3(Camera.main.transform.right.x, 0f, Camera.main.transform.right.z);
+        var camForward = new Vector3(Camera.main.transform.forward.x, 0f, Camera.main.transform.forward.z);
+        
+        Debug.Log(Camera.main.transform.rotation.eulerAngles.z);
+        
+        float headTilt = 0f;
+         if(Between(Camera.main.transform.rotation.eulerAngles.z, 0, 30)) {
+             headTilt = -Camera.main.transform.rotation.eulerAngles.z;
+         }
+         if(Between(Camera.main.transform.rotation.eulerAngles.z, 31, 180)) {
+             headTilt = -30;
+         }
+
+         if(Between(Camera.main.transform.rotation.eulerAngles.z, 330, 360)) {
+             headTilt = -(Camera.main.transform.rotation.eulerAngles.z - 360);
+         }
+
+          if(Between(Camera.main.transform.rotation.eulerAngles.z, 181, 330)) {
+             headTilt = 30;
+         }
+        headTilt *= 0.1f;
+
+        _rb.velocity = camForward * boardSpeed  + camRight * headTilt * boardHandle;
+
+        _rb.velocity += Vector3.down * 9.8f;
+
+        // ROTATION OF THE BOARD
+        Vector3 eulerRotation = new Vector3(boardObject.transform.eulerAngles.x,  Camera.main.transform.eulerAngles.y + 90, boardObject.transform.eulerAngles.z);
+        boardObject.transform.rotation = Quaternion.Euler(eulerRotation);
     }
+
+    public static bool Between(float number, float min, float max)
+{
+    return number >= min && number <= max;
+}
 }
